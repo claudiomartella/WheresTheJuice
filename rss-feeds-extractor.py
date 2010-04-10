dd23f07d35fd37eca9fa12e548288d5b1b8a69b8
Claudio Martella <claudio.martella@gmail.com>
2010-04-10 14:19:44 +0200

    first commit

---
+++ rss-feeds-extractor.py
@@ -0,0 +1,41 @@
+'''
+Created on Apr 6, 2010
+
+@author: hammer
+'''
+
+from BeautifulSoup import BeautifulSoup
+from urlparse import urljoin
+import urllib2, sys, re
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print "should pass the inputfilename"
+        sys.exit()
+        
+    for url in file(sys.argv[1]):
+        # ignore comments
+        url = url.strip()
+        if url[0] == '#':
+            continue
+        
+        #print "Processing: " + url
+        
+        html = urllib2.urlopen(url.strip())
+        soup = BeautifulSoup(html)
+        
+        res = soup.findAll('link', rel='alternate', attrs={'type': re.compile("^application/(atom|rss)\+xml")})
+        if len(res) == 0:
+            #print "Couldn't find the Feed!"
+            continue
+        
+        href = res[0]['href']
+        
+        # relative link?
+        if not href.startswith("http"):
+            link = urljoin(url, href)
+        else:
+            link = href
+        
+        print link
+
